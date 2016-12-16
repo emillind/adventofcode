6d3fcf02c597de1aaa25bfd53ac1a2874e5ae595
Emil Lindblom <emillind@kth.se>
2016-12-16 11:39:24 +0100

    Day 4 part 1 done.

--- Day 4/day4.rb
+++ Day 4/day4.rb
@@ -5,15 +5,19 @@ sum = 0
 
 lines.each do |e|
 	split = e.split('-')
-	# Pop ID and checksum
-	idAndSum = split.pop()
+	idAndSum = split.pop() # Pop ID and checksum
 	# Join rest, split into chars, group them, map on the char and then sort.
 	map = split.join.chars.group_by(&:chr).map{ |k, v| [k, v.size] }.sort_by{ |k, v| [-v,k] }
+
 	# Split the id and checksum
 	temp = idAndSum.delete "]"
 	idAndSum = temp.split('[')
-	checksum = idAndSum[1].chars
-	# Compare checksum with first 5 keys in map
+
+	checksum = idAndSum[1].chars.slice!(0,5) # The 5 characters in checksum
+	compare = map.take(5).collect(&:first) # Top 5 characters in words
+	if checksum.eql?(compare)
+		sum += Integer(idAndSum[0])
+	end
 end
 
 puts sum
